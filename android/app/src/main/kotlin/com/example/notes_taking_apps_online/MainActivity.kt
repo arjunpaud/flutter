d60d6ed369d492a6e570914fb80b18d5946595ed
Arjun Paudel <arjupau@gmail.com>
2023-01-25 16:04:45 +0545

    authentication

---
+++ android/app/src/main/kotlin/com/example/notes_taking_apps_online/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.notes_taking_apps_online
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/strings.dart
@@ -0,0 +1,9 @@
+class Strings{
+  // Image String
+  static const  image="assets/images/note.jpeg";
+  static const  google_icon="assets/images/google.png";
+
+  //Welcome String 
+  static const intro="Create and Manage\n your notes";
+  static const login="Continue with Google";
+}
